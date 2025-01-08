e344352b712173aef3e5d23c287df1b11edd2e16
takahirohonda <thon6thon6@hotmail.com>
2025-01-09 10:38:19 +1100

    feat: Add favicon and images

--- apps/next-app/src/app/layout.tsx
+++ apps/next-app/src/app/layout.tsx
@@ -3,10 +3,40 @@ import './global.css'
 import { Nav } from '../components/Nav/Nav'
 import { NextUIProviderWrapper } from '../components/NextUIProviderWrapper/NextUIProviderWrapper'
 import { WagmiProviderWrapper } from '../components/WagmiProviderWrapper/WagmiProviderWrapper'
+import { Metadata } from 'next'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'CryptoZombies',
   description: 'UI for CryptoZombies',
+  icons: {
+    apple: [
+      { url: '/favicon/apple-icon-57x57.png', sizes: '57x57' },
+      { url: '/favicon/apple-icon-60x60.png', sizes: '60x60' },
+      { url: '/favicon/apple-icon-72x72.png', sizes: '72x72' },
+      { url: '/favicon/apple-icon-76x76.png', sizes: '76x76' },
+      { url: '/favicon/apple-icon-114x114.png', sizes: '114x114' },
+      { url: '/favicon/apple-icon-120x120.png', sizes: '120x120' },
+      { url: '/favicon/apple-icon-144x144.png', sizes: '144x144' },
+      { url: '/favicon/apple-icon-152x152.png', sizes: '152x152' },
+      { url: '/favicon/apple-icon-180x180.png', sizes: '180x180' },
+    ],
+    icon: [
+      {
+        url: '/favicon/android-icon-192x192.png',
+        type: 'image/png',
+        sizes: '192x192',
+      },
+      { url: '/favicon/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
+      { url: '/favicon/favicon-96x96.png', type: 'image/png', sizes: '96x96' },
+      { url: '/favicon/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+    ],
+  },
+  manifest: '/manifest.json',
+  themeColor: '#ffffff',
+  other: {
+    'msapplication-TileColor': '#ffffff',
+    'msapplication-TileImage': '/favicon/ms-icon-144x144.png',
+  },
 }
 
 export default function RootLayout({
--- apps/next-app/src/app/page.tsx
+++ apps/next-app/src/app/page.tsx
@@ -1,3 +1,15 @@
+import Image from 'next/image'
+
 export default function Index() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <h1>CryptoZombie Playground</h1>
+      <Image
+        src="/img/zombie-192×287.png"
+        width={192}
+        height={287}
+        alt="zombie pic"
+      />
+    </>
+  )
 }
